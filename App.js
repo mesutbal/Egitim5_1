294b3e89d5e873d95bf5da5f3fd41eb309fda321
Eyup Can Dundar <eyup@ecdyazilim.com>
2019-01-29 10:30:30 +0300

    Ilk Commitin İkincisi

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { Dimensions } from 'react-native';
 import { createDrawerNavigator, createAppContainer } from 'react-navigation';
+import MainScreen from './src/screens/MainScreen';
+import SideMenu from './src/screens/SideMenu';
 
 const Drawer = createDrawerNavigator({
     Home: {
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default class MainScreen extends React.Component {
+    render() {
+        return (<View style={{ flex: 1, backgroundColor: 'yellow' }} />);
+    }
+}
